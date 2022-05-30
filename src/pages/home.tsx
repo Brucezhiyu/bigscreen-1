c212bc158ff158d7bfedca37fe4da4fbfe799fac
Brucezhiyu <lushuke5@gmail.com>
2022-05-30 14:26:38 +0800

    完成了grid 布局，更正页面宽度的公式

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,8 +3,15 @@ import './home.scss';
 import headerBg from '../images/header.png'
 export const Home = () => {
   return (
-    <div className="wrapper">
+    <div className="home">
       <header  style={{backgroundImage:`url(${headerBg})`}}></header>
+        <main>
+            <section className="section1"></section>
+            <section className="section2"></section>
+            <section className="section3"></section>
+            <section className="section4"></section>
+            <section className="section5"></section>
+        </main>
     </div>
   );
 };
