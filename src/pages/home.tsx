ccaf7cac8410ed467f888fd3a3a54e8fb344aa7b
Brucezhiyu <lushuke5@gmail.com>
2022-05-31 14:43:18 +0800

    全部完成chart4房子

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './home.scss';
 import headerBg from '../images/header.png';
 import footerBg from '../images/footer.png';
-
+import house from '../images/house.png';
 import {Chart1} from '../components/chart-1';
 import {Chart2} from '../components/chart-2';
 import {Chart3} from '../components/chart-3';
@@ -25,7 +25,9 @@ export const Home = () => {
                     <h2>小区年入住人数</h2>
                     <Chart3/>
                 </section>
-                <section className="bordered section4"></section>
+                <section className=" section4">
+                    <div className="bordered house" style={{backgroundImage: `url(${house})`}}></div>
+                </section>
                 <section className="bordered section5"></section>
             </main>
                 <footer style={{backgroundImage: `url(${footerBg})`}}></footer>
