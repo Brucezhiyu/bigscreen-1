38003521954c33832a4ee195a153ad0c31d39d75
Brucezhiyu <lushuke5@gmail.com>
2022-05-31 12:12:34 +0800

    全部完成chart2

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -9,12 +9,21 @@ export const Chart2 = () => {
 
         const myChart = echarts.init(divRef.current);
         myChart.setOption({
+            color: [
+                '#3B96FF', '#1EC919', '#BF8FF5', '#54E6F3', '#FD839C', '#F7BB03'
+            ],
             textStyle: {
                 fontSize: px(0.25),
                 color: 'gray'
             },
+            label: {
+                formatter: ' {d}%'
+            },
             legend: {
+                top: px(0.7),
                 right: 'right',
+                itemWidth: px(0.15),
+                itemHeight: px(0.15),
                 textStyle: {
                     color: '#fff',
                     fontSize: px(0.2)
@@ -25,22 +34,26 @@ export const Chart2 = () => {
                 {
                     name: 'Nightingale Chart',
                     type: 'pie',
-                    radius: [px(0.001), px(1.8)],
+                    top: px(0.4),
+                    radius: [px(0.4), px(1.8)],
                     center: ['35%', '50%'],
-                    roseType: 'area',
-                    labelLine:{
-                        length:px(0)
+                    label: {
+                        color: 'gray'
+                    },
+                    roseType: 'radius',
+                    labelLine: {
+                        length: px(0)
                     },
                     itemStyle: {
-                        borderRadius: 8
+                        borderRadius: 3
                     },
                     data: [
-                        {value: 40, name: 'rose 1'},
-                        {value: 38, name: 'rose 2'},
-                        {value: 32, name: 'rose 3'},
-                        {value: 30, name: 'rose 4'},
-                        {value: 28, name: 'rose 5'},
-                        {value: 26, name: 'rose 6'},
+                        {value: 30, name: '小区租户'},
+                        {value: 25, name: '设施维修'},
+                        {value: 8, name: '外卖快递'},
+                        {value: 12, name: '小区住户'},
+                        {value: 10, name: '环卫人员'},
+                        {value: 15, name: '小区访客'},
                     ]
                 }
             ]
